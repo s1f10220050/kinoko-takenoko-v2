971c08b08035ea005e8621f35c81132fdce96148
s1f10220050 <107381724+s1f10220050@users.noreply.github.com>
2022-06-30 18:01:18 +0900

    ka

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+kinoko=3
+takenoko=5
+message=['kinoo is wondarfull']
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    kinoko_per=kinoko/(kinoko+takenoko)*100
+    takenoko_per=takenoko/(kinoko+takenoko)*100
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
